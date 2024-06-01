3fb41d56d0ad6022f1c55882ba07081b786f93f5
Teeragit Hare <t@teeragit.com>
2024-06-01 18:29:12 +0700

    Random button colors

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-const dvpRoot = document.documentElement;
-const dvpNavToggle = document.querySelector( ".dvp-header-nav-toggle" );
+const dvp_root = document.documentElement;
+const dvp_navToggle = document.querySelector( '.dvp-header-nav-toggle' );
 
-dvpNavToggle.onclick = function() {
-	dvpRoot.classList.toggle( "dvp-header-nav-active" );
+dvp_navToggle.onclick = function() {
+	dvp_root.classList.toggle( 'dvp-header-nav-active' );
 }
+
+function dvp_randomClasses( name, prefix, variations ) {
+	let i, max;
+	const classes = document.querySelectorAll( name );
+	for ( i = 0, max = classes.length; i < max; i++ ) {
+		let x = Math.floor( ( Math.random() * variations ) + 1 );
+		classes[ i ].classList.add( prefix + x );
+	}
+}
+
+window.addEventListener( 'load', function() {
+  dvp_randomClasses( '.dvp-links-item', 'dvp-links-color-', 8 );
+})
